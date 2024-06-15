c98dad460ca6ecd9fb92143134a34d584d823b29
Ohnivkov <136961385+Ohnivkov@users.noreply.github.com>
2024-06-15 16:38:52 +0300

    Merge pull request #13 from KondratovaLudmila/dev_dmitror
    changes1

--- numberplate_ukr/train.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO("yolov8n.yaml")  # build a new model from scratch
-
-# Use the model
-results = model.train(data="data.yaml", epochs=5)  # train the model
