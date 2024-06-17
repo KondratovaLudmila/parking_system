bb6d24b946eefd9f774383f1df9234ccab42a097
Ohnivkov <136961385+Ohnivkov@users.noreply.github.com>
2024-06-17 22:40:38 +0300

    Merge pull request #13 from KondratovaLudmila/dev_dmitror
    changes1

--- parking_system/parking_system/settings.py
+++ parking_system/parking_system/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'parking_system.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
+print(os.getenv('POSTGRES_DB'))
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
