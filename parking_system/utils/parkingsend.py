e562c6a07bc79d40e40d5e0c790ee1aad629c34b
KondratovaLudmila <137877876+KondratovaLudmila@users.noreply.github.com>
2024-06-18 22:56:59 +0200

    tg notify of parking

--- parking_system/utils/parkingsend.py
+++ parking_system/utils/parkingsend.py
@@ -1,15 +1,35 @@
 import json
 import os
 import telebot
-if not os.path.exists(os.path.join(os.getcwd(), 'users.json')):
-    with open(os.path.join(os.getcwd(), 'users.json'), 'w') as f:
+from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
+WORK_DIR = Path(__file__).parent.parent.resolve()
+USERS_JSON = WORK_DIR.joinpath('bot_files', 'users.json')
+
+if not USERS_JSON.exists():
+    USERS_JSON.parent.mkdir(parents=True)
+    try:
+        with open(USERS_JSON, 'w') as f:
+            m = {}
+            json.dump(m, f)
+    
+    except Exception as err:
+        print(err)
+
+
+def tg_notify(dict_mail):
+    bot = telebot.TeleBot(os.getenv('TG_BOT_TOKEN'))
+    try:
+        with open(USERS_JSON, 'r') as f:
+            m = json.load(f)
+    
+    except Exception as err:
+        print(err)
         m = {}
-        json.dump(m, f)
-def send_mails(dict_mail):
-    user_file = os.path.join(os.getcwd(), 'users.json')
-    bot = telebot.TeleBot('6487011985:AAGxPhZuR01zCxE0uLB7JYXaTszkdy2RWpg')
-    with open(user_file, 'r') as f:
-        m = json.load(f)
-    for chat_id, email in m.items():
-        if dict_mail == email:
-            bot.send_message(chat_id, 'Ворота відкриваються.')
+
+    chat_id = m.get(dict_mail)
+    if chat_id:
+        bot.send_message(chat_id, 'Ворота відкриваються.')
